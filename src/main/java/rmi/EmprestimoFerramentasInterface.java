df8931d90e9b02ac6bae8c69e71e1cab70f34148
Bernardo Vieira <165905176+BernardoSVieira@users.noreply.github.com>
2025-11-10 11:00:43 -0300

    feat: criação da classe EmprestimoFerramentasInterface

---
+++ src/main/java/rmi/EmprestimoFerramentasInterface.java
@@ -0,0 +1,14 @@
+package rmi;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Windows
+ */
+public class EmprestimoFerramentasInterface {
+    
+}
