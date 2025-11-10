12203d75ff4749312bf6bb7ab752a84ce9007407
Bernardo Vieira <165905176+BernardoSVieira@users.noreply.github.com>
2025-11-10 11:01:51 -0300

    feat: adição de imports e operações para Amigo

--- src/main/java/rmi/EmprestimoFerramentasInterface.java
+++ src/main/java/rmi/EmprestimoFerramentasInterface.java
@@ -1,14 +1,16 @@
 package rmi;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+import modelo.Amigo;
+import modelo.Ferramenta;
+import modelo.Emprestimo;
 
-/**
- *
- * @author Windows
- */
-public class EmprestimoFerramentasInterface {
-    
-}
+public interface EmprestimoFerramentasInterface extends Remote {
+    // Operações para Amigo
+    void cadastrarAmigo(Amigo amigo) throws RemoteException;
+    void atualizarAmigo(Amigo amigo) throws RemoteException;
+    void excluirAmigo(int id) throws RemoteException;
+    Amigo buscarAmigoPorId(int id) throws RemoteException;
+    List<Amigo> listarAmigos() throws RemoteException;
