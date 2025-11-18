75d0ebcf221d62d58d130e2da3a99fbd1a0b058f
Bernardo Vieira <bernardosvieira25@gmail.com>
2025-11-18 14:06:04 -0300

    docs: FerramentaInterface
    adicionando documentação para a classe FerramentaInterface

--- src/main/java/rmi/FerramentaInterface.java
+++ src/main/java/rmi/FerramentaInterface.java
@@ -5,10 +5,21 @@ import java.rmi.RemoteException;
 import java.util.List;
 import modelo.Ferramenta;
 
+// Interface remota para operação de CRUD de ferramentas
 public interface FerramentaInterface extends Remote {
+    
+    // Cadastra nova ferranebta
     void cadastrarFerramenta(Ferramenta ferramenta) throws RemoteException;
+    
+    // Atualiza ferramenta ja existente
     void atualizarFerramenta(Ferramenta ferramenta) throws RemoteException;
+    
+    // Exclui ferramenta pelo ID
     void excluirFerramenta(int id) throws RemoteException;
+    
+    // Busca ferramenta pelo ID
     Ferramenta buscarFerramentaPorId(int id) throws RemoteException;
+    
+    //Lista todas as ferramentas cadastradas
     List<Ferramenta> listarFerramentas() throws RemoteException;
 }
